d5220ec02d35074590ba0811500d330dc9f697b8
Achille Astolfi <achille.astolfi@infocommproject.it>
2021-08-31 12:44:44 +0200

    Task service book.

--- src/main/java/com/example/library/model/book/BookResource.java
+++ src/main/java/com/example/library/model/book/BookResource.java
@@ -1,4 +1,14 @@
 package com.example.library.model.book;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// @Setter di sicuro, altrimenti non ho un punto d'ingresso per salvare i valori
+@Setter
+// @Getter dipende dalla configurazione della libreria Jackson
+@Getter
 public class BookResource {
+    private Long id;
+    private String title;
+    private String author;
 }
